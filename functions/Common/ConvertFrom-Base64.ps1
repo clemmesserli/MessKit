c5d916cf9bb705adb07a5793e44485ceb29ff1d5
Clem Messerli <clemmesserli@gmail.com>
2024-06-09 01:33:37 -0500

    edit: basic linting and addition of simple try/catch

--- functions/Common/ConvertFrom-Base64.ps1
+++ functions/Common/ConvertFrom-Base64.ps1
@@ -1,21 +1,40 @@
 Function ConvertFrom-Base64 {
+	<#
+	.SYNOPSIS
+	Converts a Base64 encoded string to plain text using the specified encoding.
+
+	.DESCRIPTION
+	This function takes a Base64 encoded string as input and decodes it to plain text using the specified encoding.
+	The default encoding is UTF8.
+
+	.PARAMETER Base64String
+	The Base64 encoded string to be decoded.
+
+	.PARAMETER Encoding
+	The encoding to be used for decoding the Base64 string.
+	Supported values: Ascii, BigEndianUnicode, BigEndianUTF32, Byte, Unicode, UTF32, UTF7, UTF8
+
+	.EXAMPLE
+	ConvertFrom-Base64 -Base64String "SGVsbG8gV29ybGQh" -Encoding UTF8
+	# Output: "Hello World"
+	#>
 	[CmdletBinding()]
 	Param (
 		[Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
-		[String]$string,
+		[String]$Base64String,
 
 		[Parameter(ValueFromPipelineByPropertyName)]
 		[ValidateSet("Ascii", "BigEndianUnicode", "BigEndianUTF32", "Byte", "Unicode", "UTF32", "UTF7", "UTF8")]
-		[String]$encoding = "UTF8"
+		[String]$Encoding = "UTF8"
 	)
 
-	Begin {}
-
 	Process {
-		$decodedBytes = [System.Convert]::FromBase64String($string)
-		$decodedText = [System.Text.Encoding]::$encoding.GetString($decodedBytes)
-		$decodedText
+		try {
+			$decodedBytes = [System.Convert]::FromBase64String($Base64String)
+			$decodedText = [System.Text.Encoding]::$Encoding.GetString($decodedBytes)
+			$decodedText
+		} catch {
+			Write-Error "Error decoding Base64 string: $_"
+		}
 	}
-
-	End {}
 }
