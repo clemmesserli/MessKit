a1b1386af36c8121b6f02c663d587f3960970568
Clem Messerli <clemmesserli@gmail.com>
2024-06-08 21:27:49 -0500

    added export options with a little help from codiumate

--- functions/Common/Get-Phonetic.ps1
+++ functions/Common/Get-Phonetic.ps1
@@ -8,153 +8,228 @@ Function Get-Phonetic {
         Legal Disclaimer:
         THIS SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
         EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
-    .PARAMETER StringList
-        One or more strings to be spelled phonetically.
     .EXAMPLE
-        Get-Phonetic -StringList "P@ssw0rd!" | Select-Object -ExpandProperty phonetic | Format-List
-        Description: Returns String plus Phonetic alphabet in json formatting
+        Get-Phonetic -Strings "P@ssw0rd!" -Export List
+        Description: Returns matching phonetic alphabet in easy to read list format
     .EXAMPLE
-        Get-Phonetic -StringList "P@ssw0rd!","Go0dL&ck^" | ConvertTo-Json -depth 10
-        Description: Returns an Array of String plus Phonetic alphabet values in json formatting
+        Get-Phonetic -Strings "P@ssw0rd!","Go0dL&ck^" -Export Json
+        Description: Returns string name, string length, and the resulting phonetics
     .EXAMPLE
-        Get-Phonetic -StringList "P@ssw0rd!","Go0dL&ck^" | ConvertTo-Json -depth 10 | ConvertFrom-Unicode
-        Description: Returns String plus Phonetic alphabet in json formatting, replacing any special unicode values resulting from the 'ConvertTo-Json' cmdlet
+        $FormatEnumerationLimit = -1
+        Get-Phonetic -Strings "P0w3rSh3!! ROcks1", "1l!|LoO0n B@$S&()" -Export raw | Out-Gridview
+        Description: Returns the raw PSObject data within a separate window
     .EXAMPLE
-        New-Password -PwdLength 16 | Get-Phonetic | ConvertTo-Json -depth 10 | ConvertFrom-Unicode
-        Description: Makes use of the 'New-Password' function to create a new randomized value which is then returned as a String plus Phonetic alphabet in json formatting, replacing any special unicode values resulting from the 'ConvertTo-Json' cmdlet
+        New-Password -PwdLength 12 | Get-Phonetic -Export audio -VoiceName David -VoiceRate 2 -VoiceVolume 25
+        Description: Call 'New-Password' function for a new randomized value which is read out loud back to user so it only exists in memory and is not recorded in bash history
+    .EXAMPLE
+        New-GUID | Get-Phonetic -Export audio
+        Description: Just another variation but this time using PowerShell built-in New-Guid cmdlet
+    .NOTES
+        Enhanced by Codiumate
     #>
     [CmdletBinding()]
     Param (
         [Parameter(Mandatory, ValueFromPipeline)]
-        [String[]]$StringList
+        [String[]]$Strings,
+
+        [Parameter()]
+        [ValidateSet('Audio', 'List', 'Json', 'Raw')]
+        [String]$Export = 'List',
+
+        [Parameter()]
+        [ValidateRange(-10, 10)]
+        [Int]$VoiceRate = 0,
+
+        [Parameter()]
+        [ValidateSet('David', 'Zira')]
+        [String]$VoiceName = 'David',
+
+        [Parameter()]
+        [ValidateRange(0, 100)]
+        [Int]$VoiceVolume = 50
     )
 
     Begin {}
 
     Process {
-        $PhoneticResponse = @()
-        ForEach ($String In $StringList) {
-            $NatoList = @()
-            foreach ($Char in $String.ToCharArray()) {
-                switch -CaseSensitive ($Char) {
-                    #region Lower-Case Letters
-                    "a" { $nato = "$Char = lower case a as in alpha" }
-                    "b" { $nato = "$Char = lower case b as in bravo" }
-                    "c" { $nato = "$Char = lower case c as in charlie" }
-                    "d" { $nato = "$Char = lower case d as in delta" }
-                    "e" { $nato = "$Char = lower case e as in echo" }
-                    "f" { $nato = "$Char = lower case f as in foxtrot" }
-                    "g" { $nato = "$Char = lower case g as in golf" }
-                    "h" { $nato = "$Char = lower case h as in hotel" }
-                    "i" { $nato = "$Char = lower case i as in india" }
-                    "j" { $nato = "$Char = lower case j as in juliett" }
-                    "k" { $nato = "$Char = lower case k as in kilo" }
-                    "l" { $nato = "$Char = lower case l as in lima" }
-                    "m" { $nato = "$Char = lower case m as in mike" }
-                    "n" { $nato = "$Char = lower case n as in november" }
-                    "o" { $nato = "$Char = lower case o as in oscar" }
-                    "p" { $nato = "$Char = lower case p as in papa" }
-                    "q" { $nato = "$Char = lower case q as in quebec" }
-                    "r" { $nato = "$Char = lower case r as in romeo" }
-                    "s" { $nato = "$Char = lower case s as in sierra" }
-                    "t" { $nato = "$Char = lower case t as in tango" }
-                    "u" { $nato = "$Char = lower case u as in uniform" }
-                    "v" { $nato = "$Char = lower case v as in victor" }
-                    "w" { $nato = "$Char = lower case w as in whiskey" }
-                    "x" { $nato = "$Char = lower case x as in x-ray" }
-                    "y" { $nato = "$Char = lower case y as in yankee" }
-                    "z" { $nato = "$Char = lower case z as in zulu" }
-                    #endregion Lower-Case Letters
+        # Define a hashtable to store NATO mappings
+        $natoMap = @{
+            LowerCaseLetters = @{
+                'a' = 'lower case a as in alpha'
+                'b' = 'lower case b as in bravo'
+                'c' = 'lower case c as in charlie'
+                'd' = 'lower case d as in delta'
+                'e' = 'lower case e as in echo'
+                'f' = 'lower case f as in foxtrot'
+                'g' = 'lower case g as in golf'
+                'h' = 'lower case h as in hotel'
+                'i' = 'lower case i as in india'
+                'j' = 'lower case j as in juliett'
+                'k' = 'lower case k as in kilo'
+                'l' = 'lower case l as in lima'
+                'm' = 'lower case m as in mike'
+                'n' = 'lower case n as in november'
+                'o' = 'lower case o as in oscar'
+                'p' = 'lower case p as in papa'
+                'q' = 'lower case q as in quebec'
+                'r' = 'lower case r as in romeo'
+                's' = 'lower case s as in sierra'
+                't' = 'lower case t as in tango'
+                'u' = 'lower case u as in uniform'
+                'v' = 'lower case v as in victor'
+                'w' = 'lower case w as in whiskey'
+                'x' = 'lower case x as in x-ray'
+                'y' = 'lower case y as in yankee'
+                'z' = 'lower case z as in zulu'
+            }
+            UpperCaseLetters = @{
+                'A' = 'upper case A as in Alpha'
+                'B' = 'upper case B as in Bravo'
+                'C' = 'upper case C as in Charlie'
+                'D' = 'upper case D as in Delta'
+                'E' = 'upper case E as in Echo'
+                'F' = 'upper case F as in Foxtrot'
+                'G' = 'upper case G as in Golf'
+                'H' = 'upper case H as in Hotel'
+                'I' = 'upper case I as in India'
+                'J' = 'upper case J as in Juliett'
+                'K' = 'upper case K as in Kilo'
+                'L' = 'upper case L as in Lima'
+                'M' = 'upper case M as in Mike'
+                'N' = 'upper case N as in November'
+                'O' = 'upper case O as in Oscar'
+                'P' = 'upper case P as in Papa'
+                'Q' = 'upper case Q as in Quebec'
+                'R' = 'upper case R as in Romeo'
+                'S' = 'upper case S as in Sierra'
+                'T' = 'upper case T as in Tango'
+                'U' = 'upper case U as in Uniform'
+                'V' = 'upper case V as in Victor'
+                'W' = 'upper case W as in Whiskey'
+                'X' = 'upper case X as in X-ray'
+                'Y' = 'upper case Y as in Yankee'
+                'Z' = 'upper case Z as in Zulu'
+            }
+            Numbers          = @{
+                '0' = 'number Zero'
+                '1' = 'number One'
+                '2' = 'number Two'
+                '3' = 'number Three'
+                '4' = 'number Four'
+                '5' = 'number Five'
+                '6' = 'number Six'
+                '7' = 'number Seven'
+                '8' = 'number Eight'
+                '9' = 'number Nine'
+            }
+            Symbols          = @{
+                '``'  = 'backtick'
+                '~'   = 'tilde'
+                '!'   = 'exclamation'
+                '@'   = 'at sign'
+                '#'   = 'pound'
+                '`$'  = 'dollar'
+                '%'   = 'percent'
+                '^'   = 'carat'
+                '&'   = 'ampersand'
+                '<'   = 'less than'
+                '>'   = 'greater than'
+                '='   = 'equal'
+                '+'   = 'plus'
+                '-'   = 'minus'
+                '*'   = 'asterisk'
+                '/'   = 'forward slash'
+                '\'   = 'back slash'
+                '.'   = 'period'
+                ','   = 'comma'
+                ';'   = 'semicolon'
+                ':'   = 'colon'
+                '_'   = 'underscore'
+                '('   = 'left parenthesis'
+                ')'   = 'right parenthesis'
+                '{'   = 'left curly brace'
+                '}'   = 'right curly brace'
+                '['   = 'left bracket'
+                ']'   = 'right bracket'
+                '|'   = 'pipe'
+                '`''' = 'single quote'
+                '`"'  = 'double quote'
+                '?'   = 'question mark'
+                ' '   = 'space'
+            }
+        }
 
-                    #region Upper-Case Letters
-                    "A" { $nato = "$Char = upper case A as in Alpha" }
-                    "B" { $nato = "$Char = upper case A as in Bravo" }
-                    "C" { $nato = "$Char = upper case C as in Charlie" }
-                    "D" { $nato = "$Char = upper case D as in Delta" }
-                    "E" { $nato = "$Char = upper case E as in Echo" }
-                    "F" { $nato = "$Char = upper case F as in Foxtrot" }
-                    "G" { $nato = "$Char = upper case G as in Golf" }
-                    "H" { $nato = "$Char = upper case H as in Hotel" }
-                    "I" { $nato = "$Char = upper case I as in India" }
-                    "J" { $nato = "$Char = upper case J as in Juliett" }
-                    "K" { $nato = "$Char = upper case K as in Kilo" }
-                    "L" { $nato = "$Char = upper case L as in Lima" }
-                    "M" { $nato = "$Char = upper case M as in Mike" }
-                    "N" { $nato = "$Char = upper case N as in November" }
-                    "O" { $nato = "$Char = upper case O as in Oscar" }
-                    "P" { $nato = "$Char = upper case P as in Papa" }
-                    "Q" { $nato = "$Char = upper case Q as in Quebec" }
-                    "R" { $nato = "$Char = upper case R as in Romeo" }
-                    "S" { $nato = "$Char = upper case S as in Sierra" }
-                    "T" { $nato = "$Char = upper case T as in Tango" }
-                    "U" { $nato = "$Char = upper case U as in Uniform" }
-                    "V" { $nato = "$Char = upper case V as in Victor" }
-                    "W" { $nato = "$Char = upper case W as in Whiskey" }
-                    "X" { $nato = "$Char = upper case X as in X-ray" }
-                    "Y" { $nato = "$Char = upper case Y as in Yankee" }
-                    "Z" { $nato = "$Char = upper case Z as in Zulu" }
-                    #endregion Upper-Case Letters
-
-                    #region Numbers
-                    "0" { $nato = "$Char = number Zero" }
-                    "1" { $nato = "$Char = number One" }
-                    "2" { $nato = "$Char = number Two" }
-                    "3" { $nato = "$Char = number Three" }
-                    "4" { $nato = "$Char = number Four" }
-                    "5" { $nato = "$Char = number Five" }
-                    "6" { $nato = "$Char = number Six" }
-                    "7" { $nato = "$Char = number Seven" }
-                    "8" { $nato = "$Char = number Eight" }
-                    "9" { $nato = "$Char = number Nine" }
-                    #endregion Numbers
-
-                    #region Symbols
-                    "``" { $nato = "$Char = backtick" }
-                    "~" { $nato = "$Char = tilde" }
-                    "!" { $nato = "$Char = exclamation" }
-                    "@" { $nato = "$Char = at sign" }
-                    "#" { $nato = "$Char = pound" }
-                    "`$" { $nato = "$Char = dollar" }
-                    "%" { $nato = "$Char = percent" }
-                    "^" { $nato = "$Char = carat" }
-                    "&" { $nato = "$Char = ampersand" }
-                    "<" { $nato = "$Char = less than" }
-                    ">" { $nato = "$Char = greater than" }
-                    "=" { $nato = "$Char = equal" }
-                    "+" { $nato = "$Char = plus" }
-                    "-" { $nato = "$Char = minus" }
-                    "*" { $nato = "$Char = asterisk" }
-                    "/" { $nato = "$Char = forward slash" }
-                    "\" { $nato = "$Char = back slash" }
-                    "." { $nato = "$Char = period" }
-                    "," { $nato = "$Char = comma" }
-                    ";" { $nato = "$Char = semicolon" }
-                    ":" { $nato = "$Char = colon" }
-                    "_" { $nato = "$Char = underscore" }
-                    "(" { $nato = "$Char = left parenthesis" }
-                    ")" { $nato = "$Char = right parenthesis" }
-                    "{" { $nato = "$Char = left curly brace" }
-                    "}" { $nato = "$Char = right curly brace" }
-                    "[" { $nato = "$Char = left bracket" }
-                    "]" { $nato = "$Char = right bracket" }
-                    "|" { $nato = "$Char = pipe" }
-                    "`'" { $nato = "$Char = single quote" }
-                    "`"" { $nato = "$Char = double quote" }
-                    "?" { $nato = "$Char = question mark" }
-                    " " { $nato = "$Char = space" }
-                    #endregion Symbols
+        $phoneticList = New-Object System.Collections.Generic.List[System.Object]
+        ForEach ($string In $Strings) {
+            #$string = 'P0w3rSh3!l'
+            $natoList = New-Object System.Collections.Generic.List[System.Object]
+            ForEach ($char in $string.ToCharArray()) {
+                switch ($char) {
+                    { $natoMap.LowerCaseLetters.Keys -ccontains $_ } {
+                        $natoList.add("$char = $($natoMap.LowerCaseLetters["$char"])")
+                    }
+                    { $natoMap.UpperCaseLetters.Keys -ccontains $_ } {
+                        $natoList.add("$char = $($natoMap.UpperCaseLetters["$char"])")
+                    }
+                    { $natoMap.Numbers.Keys -ccontains $_ } {
+                        $natoList.add("$char = $($natoMap.Numbers["$char"])")
+                    }
+                    { $natoMap.Symbols.Keys -ccontains $_ } {
+                        $natoList.add("$char = $($natoMap.Symbols["$char"])")
+                    }
+                    default {
+                        $natoList.add("$char = No mapping found")
+                    }
                 }
-                $NatoList += $nato
-                $NatoHash = [PSCustomObject]@{
-                    String   = $String;
-                    Length   = $String.Length;
-                    Phonetic = $NatoList;
+                $natoHash = [PSCustomObject]@{
+                    String   = $string
+                    Length   = $string.Length
+                    Phonetic = $natoList
                 }
             }
-            $PhoneticResponse += $NatoHash
+            $phoneticList.Add($natoHash)
         }
-        $PhoneticResponse
-    }
 
-    End {}
+        switch ($Output) {
+            'Audio' {
+                # Use the built-in "Add-Type" cmdlet to load the System.Speech assembly
+                Add-Type -AssemblyName System.Speech
+
+                # Create a SpeechSynthesizer object
+                $synthesizer = New-Object System.Speech.Synthesis.SpeechSynthesizer
+
+                # You can list available voices using: $synthesizer.GetInstalledVoices().VoiceInfo
+                switch ($VoiceName) {
+                    'David' { $synthesizer.SelectVoice("Microsoft David Desktop") }
+                    'Zira' { $synthesizer.SelectVoice("Microsoft Zira Desktop") }
+                }
+
+                # The default rate is 0 (normal speed); positive values speed up, negative values slow down
+                $synthesizer.Rate = $VoiceRate
+
+                # The volume you wish to set for the speaker output.
+                $synthesizer.Volume = $VoiceVolume
+
+                # Speak the phrase after trimming off the leading '$char ='
+                $synthesizer.Speak('Preparing Text to Speech')
+                Start-Sleep -Milliseconds 30
+                $phoneticList.phonetic | ForEach-Object {
+                    $synthesizer.Speak($_.Substring(3))
+                }
+
+                $synthesizer.Dispose()
+            }
+            'Json' {
+                $phoneticList | ConvertTo-Json -Depth 1
+            }
+            'List' {
+                ($phoneticList).phonetic
+            }
+            'Raw' {
+                $phoneticList
+            }
+        }
+    }
 }
+
+End {}
