1ee5f24e81b824f60213b8ec30ef5155c4284b4e
Clem Messerli <clemmesserli@gmail.com>
2024-06-14 18:21:52 -0500

    fix: add logic to auto-retry up to 5x if request gets throttled

--- functions/Common/ConvertFrom-Geocode.ps1
+++ functions/Common/ConvertFrom-Geocode.ps1
@@ -1,61 +1,76 @@
 Function ConvertFrom-Geocode {
 	<#
-	.SYNOPSIS
-		translate latitude and longitude coordinates into an address
-	.DESCRIPTION
-		This function calls a free ThirdParty webservice which will throttle results so a sleep timer has been added to help
-	.EXAMPLE
-		ConvertFrom-Geocode -coordinateList @(
-			@{Long = -103.45846; Lat = 43.87475},
-			@{Long = -112.13094; Lat = 36.05137}
+		.SYNOPSIS
+		Translate latitude and longitude coordinates into an address.
+
+		.DESCRIPTION
+		This function calls a free ThirdParty webservice which may throttle results, so a sleep timer has been added to help manage the requests.
+
+		.EXAMPLE
+		$GeoData = ConvertFrom-Geocode -Coordinates @(
+			@{Lat = 40.24695; Lon = -89.77191},
+			@{Lat = 41.90313; Lon = 12.45384},
+			@{Lat = 40.6892; Lon = -74.0445}
 		) -verbose
 	#>
 	[CmdletBinding()]
 	Param (
-		[hashtable[]]$coordinateList,
-
-		[String]$url = "https://geocode.xyz/"
+		[Parameter(Mandatory, ValueFromPipeline)]
+		[hashtable[]]$Coordinates
 	)
 
-	Begin {}
-
 	Process {
-		Write-Verbose "Creating a web session"
-		if (!($session)) {
-			$null = Invoke-RestMethod -Uri $url -SessionVariable session
-		}
-
-		$total = $coordinateList.count
+		$total = $Coordinates.count
 		$count = 0
-		$coordinateList | ForEach-Object {
+		$results = [System.Collections.Generic.List[PSObject]]::New()
+		$url = "https://geocode.xyz"
+		$null = Invoke-RestMethod -Uri $url -SessionVariable geoSession
+
+		$Coordinates | ForEach-Object {
 			$count++
-			if ($count -ne $total) {
-				Write-Verbose "Sleeping for $($sleepTimer) seconds..."
-				Start-Sleep -Seconds (10..30 | Get-Random )
+			$lat = $_.lat
+			$lon = $_.lon
+
+			# Validate latitude and longitude
+			if ($lat -ge -90 -and $lat -le 90 -and $lon -ge -180 -and $lon -le 180) {
+				Write-Verbose "Now resolving Lat=$lat, Lon=$lon ($count of $total) `n"
+			} else {
+				Write-Warning "Invalid coordinates: Lat=$lat, Lon=$lon"
 			}
-			Write-Verbose "Now resolving $_  ($count of $total) `n"
-			$coordinate = $_.lat, $_.long -join ","
-			$response = Invoke-RestMethod "$url/${coordinate}?geoit=json" -WebSession $session
-			$response | ForEach-Object {
-				[PSCustomObject]@{
-					Address          = $("$($response.stnumber) $($response.staddress)")
-					City             = $response.city
-					State            = $response.state
-					ZipCode          = $response.postal
-					Province         = $response.prov
-					Country          = $response.country
-					Latitude         = $response.latt
-					Longitude        = $response.longt
-					Timezone         = $response.timezone
-					Elevation        = $response.elevation
-					County           = $response.adminareas.admin6.alt_name
-					Population       = $response.adminareas.admin6.population
-					Confidence       = $response.confidence
-					PointsOfInterest = $response.poi
+
+			try {
+				$response = Invoke-RestMethod -Uri "$url/$lat,$lon?json=1&moreinfo=1" -WebSession $geoSession
+
+				$maxAttempts = 5
+				$attempt = 0
+				while ($response.latt -match 'Throttled' -and $attempt -lt $maxAttempts) {
+					Write-Verbose "Request Throttled...Retry [$attempt] of [$maxAttempts] for: [$lat, $lon]"
+					Start-Sleep -Seconds (5..30 | Get-Random )
+					$response = Invoke-RestMethod -Uri "$url/$lat,$lon?json=1&moreinfo=1" -WebSession $geoSession
+					$attempt++
 				}
+
+				$results.Add(
+					[PSCustomObject]@{
+						Address          = "$($response.stnumber) $($response.staddress)"
+						City             = $response.city
+						State            = $response.statename
+						StateCode        = $response.state
+						ZipCode          = $response.postal
+						Country          = $response.country
+						CountryCode      = $response.prov
+						Latitude         = $response.latt
+						Longitude        = $response.longt
+						Timezone         = $response.timezone
+						Elevation        = $response.elevation
+						Confidence       = $response.confidence
+						PointsOfInterest = $response.poi
+					}
+				)
+			} catch {
+				Write-Error "Error occurred while invoking the REST method: $_"
 			}
 		}
+		$results
 	}
-
-	End {}
 }
--- functions/Common/ConvertFrom-Unicode.ps1
+++ functions/Common/ConvertFrom-Unicode.ps1
@@ -1,17 +1,33 @@
 Function ConvertFrom-Unicode {
+    <#
+        .SYNOPSIS
+        Converts Unicode escape sequences in a string to their corresponding Unicode characters.
+
+        .DESCRIPTION
+        This function takes a string as input and converts any Unicode escape sequences (e.g. \uXXXX) to their corresponding Unicode characters.
+
+        .PARAMETER InputString
+        The string containing Unicode escape sequences to be converted.
+
+        .EXAMPLE
+        ConvertFrom-Unicode -InputString "Hello \u0048\u0065\u006C\u006C\u006F"
+        Output: "Hello Hello"
+
+        .EXAMPLE
+        "Hello \u0048\u0065\u006C\u006C\u006F" | ConvertFrom-Unicode
+        Output: "Hello Hello"
+
+        This example converts the Unicode escape sequences in the input string to their corresponding characters.
+    #>
     [CmdletBinding()]
     Param (
-        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
-        [string] $string
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [string] $InputString
     )
 
-    Begin {}
-
     Process {
-        $string | ForEach-Object {
+        $InputString | ForEach-Object {
             [System.Text.RegularExpressions.Regex]::Unescape($_)
         }
     }
-
-    End {}
 }
