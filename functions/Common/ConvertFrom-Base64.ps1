844d1b1a42ba2ad65e29f8691330715e8bde7641
Clem Messerli <clemmesserli@gmail.com>
2024-06-14 18:20:48 -0500

    edit: update help indentation for consistency

--- functions/Common/ConvertFrom-Base64.ps1
+++ functions/Common/ConvertFrom-Base64.ps1
@@ -1,22 +1,22 @@
 Function ConvertFrom-Base64 {
 	<#
-	.SYNOPSIS
-	Converts a Base64 encoded string to plain text using the specified encoding.
+		.SYNOPSIS
+		Converts a Base64 encoded string to plain text using the specified encoding.
 
-	.DESCRIPTION
-	This function takes a Base64 encoded string as input and decodes it to plain text using the specified encoding.
-	The default encoding is UTF8.
+		.DESCRIPTION
+		This function takes a Base64 encoded string as input and decodes it to plain text using the specified encoding.
+		The default encoding is UTF8.
 
-	.PARAMETER Base64String
-	The Base64 encoded string to be decoded.
+		.PARAMETER Base64String
+		The Base64 encoded string to be decoded.
 
-	.PARAMETER Encoding
-	The encoding to be used for decoding the Base64 string.
-	Supported values: Ascii, BigEndianUnicode, BigEndianUTF32, Byte, Unicode, UTF32, UTF7, UTF8
+		.PARAMETER Encoding
+		The encoding to be used for decoding the Base64 string.
+		Supported values: Ascii, BigEndianUnicode, BigEndianUTF32, Byte, Unicode, UTF32, UTF7, UTF8
 
-	.EXAMPLE
-	ConvertFrom-Base64 -Base64String "SGVsbG8gV29ybGQh" -Encoding UTF8
-	# Output: "Hello World"
+		.EXAMPLE
+		ConvertFrom-Base64 -Base64String "SGVsbG8gV29ybGQh" -Encoding UTF8
+		# Output: "Hello World"
 	#>
 	[CmdletBinding()]
 	Param (
