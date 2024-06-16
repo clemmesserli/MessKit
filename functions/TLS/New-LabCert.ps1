db9979947d4d5a7caaec1446e0f2cfbe6287756f
Clem Messerli <clemmesserli@gmail.com>
2024-06-16 13:18:26 -0500

    lint: restructured to make a bit more readable and fixed some of the KeyUsage info

--- functions/TLS/New-LabCert.ps1
+++ functions/TLS/New-LabCert.ps1
@@ -1,206 +1,200 @@
 Function New-LabCert {
     <#
-	.SYNOPSIS
-		Create various certs that may be used as part of a lab
-	.DESCRIPTION
-		This function can be used to create Root, Issuer, Web, CodeSigning, and Document Encryption certificates
-	.EXAMPLE
-		New-LabCert -CertType 'Root' -CertStore 'CurrentUser' -Subject 'MyRootCA' -Days '365'
-	.EXAMPLE
-		New-LabCert -CertType 'Intermediate' -CertStore 'CurrentUser' -Subject 'MyIntermediateCA' -Days '365' -Issuer 'MyRootCA'
-	.EXAMPLE
-		New-LabCert -CertType 'DocumentEncryption' -CertStore 'CurrentUser' -Subject 'MyCryptoCert' -Days '365' -Issuer 'MyIntermediateCA'
-	.EXAMPLE
-		New-LabCert -CertType 'Web' -CertStore 'CurrentUser' -Subject 'demo1.mylab.com' -Days '365' -Issuer 'MyIntermediateCA'
- 	.EXAMPLE
-		New-LabCert -CertType 'ClientAuth' -CertStore 'CurrentUser' -Subject 'CN=HelpDeskUser,OU=Users,DC=mylab,DC=com' -UPN 'HelpDeskUser@mylab.com' -Days '30' -TargetDomain 'demo1.mylab.com'
+        .SYNOPSIS
+        Create various certs that may be used as part of a lab
+
+        .DESCRIPTION
+        This function can be used to create Root, Issuer, Web, CodeSigning, and Document Encryption certificates
+
+        .EXAMPLE
+        New-LabCert -CertType "Root" -Subject "MyLabRootCA" -Days 365
+
+        .EXAMPLE
+        New-LabCert -CertType "Intermediate" -Subject "MyLabIssuerCA" -Days 365 -Issuer "MyLabRootCA"
+
+        .EXAMPLE
+        New-LabCert -CertType "Web" -Subject "demo1.mylab.com" -Days 365 -Issuer "MyLabIssuerCA"
+
+        .EXAMPLE
+        New-LabCert -CertType "CodeSigning" -CertStore "CurrentUser" -Subject "MyLabCodeSign" -Days 365 -Issuer "MyLabIssuerCA"
+
+        .EXAMPLE
+        New-LabCert -CertType "DocumentEncryption" -CertStore "CurrentUser" -Subject "MyLabDocEncryption" -Days 365 -Issuer "MyLabIssuerCA" -NonExportable
+
+        .EXAMPLE
+        $certParams = @{
+            CertStore = "CurrentUser"
+            CertType = "ClientAuth"
+            Days = 30
+            NonExportable = $true
+            Issuer = "MyLabIssuerCA"
+            Subject = "CN=HelpDeskUser,OU=Users,DC=mylab,DC=com"
+            TargetDomain = "demo1.mylab.com"
+            UPN = "HelpDeskUser@mylab.com"
+        }
+        New-LabCert @certParams
 	#>
     [CmdletBinding()]
     Param (
-        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
-        [Aliast("SubjectName")]
-        [String]$Subject,
+        [Parameter(Mandatory)]
+        [Alias("SubjectName")]
+        [string]$Subject,
 
-        [Parameter(ValueFromPipelineByPropertyName)]
+        [Parameter()]
         [ValidateSet("LocalMachine", "CurrentUser")]
-        [String]$CertStore = "CurrentUser",
+        [string]$CertStore = "LocalMachine",
 
-        [Parameter(ValueFromPipelineByPropertyName)]
-        [String[]]$SubjectAltName,
+        [Parameter()]
+        [string[]]$SubjectAltName,
 
-        [Parameter(ValueFromPipelineByPropertyName)]
+        [Parameter()]
         [Alias("UPN")]
-        [String[]]$UserPrincipalName,
+        [string[]]$UserPrincipalName,
 
-        [Parameter(ValueFromPipelineByPropertyName)]
-        [String[]]$GUID = (New-Guid).Guid,
+        [Parameter()]
+        [string[]]$GUID = (New-Guid).Guid,
 
-        [Parameter(ValueFromPipelineByPropertyName)]
-        [String[]]$TargetDomain,
+        [Parameter()]
+        [string[]]$TargetDomain,
 
-        [Parameter(ValueFromPipelineByPropertyName)]
-        [String]$Issuer,
+        [Parameter()]
+        [string]$Issuer,
 
-        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [Parameter(Mandatory)]
         [ValidateSet("Root", "Intermediate", "Web", "CodeSigning", "DocumentEncryption", "ClientAuth")]
-        [String]$CertType,
+        [string]$CertType,
 
-        [Parameter(ValueFromPipelineByPropertyName)]
-        [Int]$Days = 365
+        [Parameter()]
+        [int]$Days = 365,
+
+        [Parameter()]
+        [switch]$NonExportable
     )
 
-    Begin {}
-
     Process {
-        $NotAfter = (Get-Date).AddDays($Days)
-        $DnsName = @([Array]$Subject + [Array]$SubjectAltName) | Sort-Object -Unique
+        $notAfter = (Get-Date).AddDays($Days)
+        $dnsName = @([Array]$Subject + [Array]$SubjectAltName) | Sort-Object -Unique
+        if ($NonExportable) {
+            $keyExportPolicy = "NonExportable"
+        } else {
+            $keyExportPolicy = "Exportable"
+        }
+        $commonParams = @{
+            CertStoreLocation = $(Join-Path "Cert:\$CertStore" "My")
+            HashAlgorithm     = "sha256"
+            KeyAlgorithm      = "RSA"
+            KeyExportPolicy   = "$keyExportPolicy"
+            KeyLength         = 2048
+            NotAfter          = $notAfter
+            Subject           = "$Subject"
+        }
 
         Switch ($CertType) {
             "Root" {
-                $CertParams = @{
-                    CertStoreLocation = $(Join-Path "Cert:\$CertStore" "My")
-                    HashAlgorithm     = "sha256"
-                    KeyAlgorithm      = "RSA"
-                    KeyExportPolicy   = "Exportable"
-                    KeyLength         = 2048
-                    KeySpec           = "Signature"
-                    KeyUsage          = @(
+                $certParams = @{
+                    DnsName       = "$Subject"
+                    KeySpec       = "Signature"
+                    KeyUsage      = @(
                         "CRLSign",
-                        "CertSign",
-                        "DataEncipherment",
-                        "DigitalSignature",
-                        "KeyAgreement",
-                        "KeyEncipherment"
+                        "CertSign"
                     )
-                    KeyUsageProperty  = "Sign"
-                    Provider          = "Microsoft Strong Cryptographic Provider"
-                    Subject           = "$Subject"
-                    TextExtension     = @(
-                        "2.5.29.37={text}1.3.6.1.5.5.7.3.1,1.3.6.1.5.5.7.3.2,1.3.6.1.5.5.7.3.3,1.3.6.1.5.5.7.3.4,1.3.6.1.5.5.7.3.8,1.3.6.1.4.1.311.10.3.4,1.3.6.1.4.1.311.10.3.12",
+                    Provider      = "Microsoft Strong Cryptographic Provider"
+                    TextExtension = @(
                         "2.5.29.19={critical}{text}cA=true&pathLength=1"
                     )
-                    Type              = "Custom"
-                    DnsName           = "$Subject"
-                    NotAfter          = $NotAfter
+                    Type          = "Custom"
                 }
-                New-SelfSignedCertificate @CertParams
+                $rootCert = New-SelfSignedCertificate @commonParams @certParams
+                # Move the certificate to the Trusted Root Certification Authorities store
+                $destStore = New-Object System.Security.Cryptography.X509Certificates.X509Store("Root", $CertStore)
+                $destStore.Open("ReadWrite")
+                $destStore.Add($rootCert)
+                $destStore.Close()
+                $rootCert
             }
             "Intermediate" {
-                $Signer = ( Get-ChildItem $(Join-Path "Cert:\$CertStore" "My") | Where-Object Subject -EQ "CN=$Issuer" )
-
-                $CertParams = @{
-                    CertStoreLocation = $(Join-Path "Cert:\$CertStore" "My")
-                    HashAlgorithm     = "sha256"
-                    KeyAlgorithm      = "RSA"
-                    KeyExportPolicy   = "Exportable"
-                    KeyLength         = 2048
-                    KeySpec           = "Signature"
-                    KeyUsage          = @(
+                $Signer = ( Get-ChildItem $(Join-Path "Cert:\$CertStore" "My") | Where-Object Subject -EQ "CN=$Issuer" | Select-Object -Last 1 )
+                $certParams = @{
+                    DnsName       = "$Subject"
+                    KeySpec       = "Signature"
+                    KeyUsage      = @(
                         "CRLSign",
-                        "CertSign",
-                        "DataEncipherment",
-                        "DigitalSignature",
-                        "KeyAgreement",
-                        "KeyEncipherment"
+                        "CertSign"
                     )
-                    KeyUsageProperty  = "Sign"
-                    Provider          = "Microsoft Strong Cryptographic Provider"
-                    Subject           = "$Subject"
-                    TextExtension     = @(
-                        "2.5.29.37={text}1.3.6.1.5.5.7.3.1,1.3.6.1.5.5.7.3.2,1.3.6.1.5.5.7.3.3,1.3.6.1.5.5.7.3.4,1.3.6.1.5.5.7.3.8,1.3.6.1.4.1.311.10.3.4,1.3.6.1.4.1.311.10.3.12",
+                    Provider      = "Microsoft Strong Cryptographic Provider"
+                    Signer        = $Signer
+                    TextExtension = @(
                         "2.5.29.19={critical}{text}cA=true&pathLength=0"
                     )
-                    Type              = "Custom"
-                    DnsName           = "$Subject"
-                    NotAfter          = $NotAfter
-                    Signer            = $Signer
+                    Type          = "Custom"
                 }
-                New-SelfSignedCertificate @CertParams
+                New-SelfSignedCertificate @commonParams @certParams
             }
             "Web" {
                 $Signer = ( Get-ChildItem $(Join-Path "Cert:\$CertStore" "CA") | Where-Object Subject -EQ "CN=$Issuer" )
-
-                $CertParams = @{
-                    CertStoreLocation = $(Join-Path "Cert:\$CertStore" "My")
-                    HashAlgorithm     = "sha256"
-                    KeyAlgorithm      = "RSA"
-                    KeyExportPolicy   = "Exportable"
-                    KeyLength         = 2048
-                    Provider          = "Microsoft Strong Cryptographic Provider"
-                    Subject           = "$Subject"
-                    TextExtension     = @(
+                $certParams = @{
+                    DnsName       = $dnsName
+                    FriendlyName  = "$($Subject)_$($notAfter.ToString('yyyyMMdd'))"
+                    KeyUsage      = @(
+                        "DigitalSignature",
+                        "KeyEncipherment"
+                    )
+                    Provider      = "Microsoft Strong Cryptographic Provider"
+                    Signer        = $Signer
+                    TextExtension = @(
                         "2.5.29.37={text}1.3.6.1.5.5.7.3.1,1.3.6.1.5.5.7.3.2",
                         "2.5.29.19={text}cA=false"
                     )
-                    DnsName           = $DnsName
-                    FriendlyName      = "$($Subject)_$($NotAfter.ToString('yyyyMMdd'))"
-                    NotAfter          = $NotAfter
-                    Signer            = $Signer
                 }
-                New-SelfSignedCertificate @CertParams
+                New-SelfSignedCertificate @commonParams @certParams
             }
             "ClientAuth" {
                 $Signer = ( Get-ChildItem $(Join-Path "Cert:\$CertStore" "CA") | Where-Object Subject -EQ "CN=$Issuer" )
-
                 $CertParams = @{
-                    Type              = "Custom"
-                    CertStoreLocation = $(Join-Path "Cert:\$CertStore" "My")
-                    HashAlgorithm     = "sha256"
-                    KeyAlgorithm      = "RSA"
-                    KeyExportPolicy   = "NonExportable"
-                    KeyLength         = 2048
-                    KeyUsage          = "DigitalSignature"
-                    Subject           = "$Subject"
-                    TextExtension     = @(
+                    KeyUsage      = @("DigitalSignature")
+                    Provider      = "Microsoft Software Key Storage Provider"
+                    Signer        = $Signer
+                    TextExtension = @(
                         "2.5.29.37={text}1.3.6.1.5.5.7.3.2",
                         "2.5.29.17={text}UPN=$UserPrincipalName&DirectoryName=$Subject&GUID=$GUID&URL=$TargetDomain"
                     )
-                    NotAfter          = $NotAfter
-                    Signer            = $Signer
+                    Type          = "Custom"
                 }
-                New-SelfSignedCertificate @CertParams
+                New-SelfSignedCertificate @commonParams @CertParams
             }
             "DocumentEncryption" {
                 $Signer = ( Get-ChildItem $(Join-Path "Cert:\$CertStore" "CA") | Where-Object Subject -EQ "CN=$Issuer" )
-
-                $CertParams = @{
-                    Type              = "DocumentEncryptionCert"
-                    CertStoreLocation = $(Join-Path "Cert:\$CertStore" "My")
-                    FriendlyName      = "$($Subject)_$($NotAfter.ToString('yyyyMMdd'))"
-                    HashAlgorithm     = "sha256"
-                    KeyAlgorithm      = "RSA"
-                    KeyExportPolicy   = "NonExportable"
-                    KeyLength         = 2048
-                    KeyUsage          = "KeyEncipherment", "KeyAgreement", "DataEncipherment", "DigitalSignature"
-                    Provider          = "Microsoft Strong Cryptographic Provider"
-                    Subject           = "$Subject"
-                    TextExtension     = @(
+                $certParams = @{
+                    FriendlyName  = "$($Subject)_$($notAfter.ToString('yyyyMMdd'))"
+                    KeyUsage      = @(
+                        "KeyEncipherment",
+                        "KeyAgreement",
+                        "DataEncipherment",
+                        "DigitalSignature"
+                    )
+                    Provider      = "Microsoft Strong Cryptographic Provider"
+                    Signer        = $Signer
+                    TextExtension = @(
                         "2.5.29.37={text}1.3.6.1.4.1.311.80.1"
                     )
-                    NotAfter          = $NotAfter
-                    Signer            = $Signer
+                    Type          = "DocumentEncryptionCert"
                 }
-                New-SelfSignedCertificate @CertParams
+                New-SelfSignedCertificate @commonParams @certParams
             }
             "CodeSigning" {
                 $Signer = ( Get-ChildItem $(Join-Path "Cert:\$CertStore" "CA") | Where-Object Subject -EQ "CN=$Issuer" )
-
-                $CertParams = @{
-                    Type              = "CodeSigningCert"
-                    CertStoreLocation = $(Join-Path "Cert:\$CertStore" "My")
-                    Subject           = "$Subject"
-                    FriendlyName      = "$($Subject)_$($NotAfter.ToString('yyyyMMdd'))"
-                    HashAlgorithm     = "sha256"
-                    KeyAlgorithm      = "RSA"
-                    KeyExportPolicy   = "NonExportable"
-                    KeyLength         = 2048
-                    NotAfter          = $NotAfter
-                    Signer            = $Signer
+                $certParams = @{
+                    FriendlyName  = "$($Subject)_$($notAfter.ToString('yyyyMMdd'))"
+                    KeyUsage      = @("DigitalSignature")
+                    Provider      = "Microsoft Strong Cryptographic Provider"
+                    Signer        = $Signer
+                    TextExtension = @(
+                        "2.5.29.37={text}1.3.6.1.5.5.7.3.3"
+                    )
+                    Type          = "CodeSigningCert"
                 }
-                New-SelfSignedCertificate @CertParams
+                New-SelfSignedCertificate @commonParams @certParams
             }
         }
     }
-
-    End {}
 }
