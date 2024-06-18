57f3e2ed6463d9313958524484aea8dfca5773fa
Clem Messerli <clem.messerli@transamerica.com>
2024-06-18 15:22:00 -0500

    edit: exclude pester tests from import-module

--- MessKit.psm1
+++ MessKit.psm1
@@ -4,7 +4,7 @@ Param ()
 Set-Location -Path $PSScriptRoot
 
 if (Test-Path -Path "$PSScriptRoot\functions") {
-    Get-ChildItem -Recurse -Path $PSScriptRoot\functions\*.ps1 | ForEach-Object -Process {
+    Get-ChildItem -Recurse -Path $PSScriptRoot\functions\*.ps1 -Exclude '*.Tests.ps1' | ForEach-Object -Process {
         . $_.FullName
     }
 }
