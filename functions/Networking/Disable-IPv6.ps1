d1e515248ca2edd9d99dd8b31632028c59e91e8e
Clem Messerli <clemmesserli@gmail.com>
2024-06-22 15:00:21 -0500

    lint: convert tab to space indent and punctuation for better viewing consistency

--- functions/Networking/Disable-IPv6.ps1
+++ functions/Networking/Disable-IPv6.ps1
@@ -1,54 +1,67 @@
 function Disable-IPv6 {
-	<#
-	.SYNOPSIS
-		Disable the IPv6 protocol
-	.DESCRIPTION
-		Disable the IPv6 protocol
-	.EXAMPLE
-		Disable-IPv6
-	.EXAMPLE
-		Disable-IPv6 -Name "Wi-Fi"
-	.EXAMPLE
-		Disable-IPv6 -AdapterName "Local Area Connection"
-	#>
-	[CmdletBinding()]
-	param (
-		[Alias("AdapterName")]
-		[string[]] $Name = "all"
-	)
+  <#
+  .SYNOPSIS
+  Disable the IPv6 protocol.
 
-	begin {
-		if (-not (Test-Administrator)) {
-			Write-Error "This script must be executed as Administrator."
-			Break
-		}
+  .DESCRIPTION
+  Disable the IPv6 protocol on specified network adapters or all adapters if no specific adapter is provided.
 
-		if (-not(Get-Module -Name "netadapter")) {
-			try {
-				Get-ModuleDependency -name "netadapter"
-			} catch {
-				Throw "Unable to load dependent module"
-				Break
-			}
-		}
-	}
+  .EXAMPLE
+  Disable-IPv6
 
-	process {
-		if ($Name.tolower() -eq "all") {
-			try {
-				Disable-NetAdapterBinding -Name * -ComponentID ms_tcpip6 -PassThru
-			} catch {
-				Throw "Unable to update all NetAdapterBindings"
-			}
-		} else {
-			$Name | ForEach-Object {
-				try {
-					Disable-NetAdapterBinding -Name $_ -ComponentID "ms_tcpip6" -PassThru
-				} catch {
-					Throw "Unable to update $_ NetAdapterBinding"
-				}
-			}
-		}
-		Get-NetAdapterBinding -Name * -ComponentID "ms_tcpip6"
-	}
+  .EXAMPLE
+  Disable-IPv6 -Name "Wi-Fi"
+
+  .EXAMPLE
+  Disable-IPv6 -Name "Local Area Connection"
+  #>
+  [CmdletBinding()]
+  param (
+    [Alias("AdapterName")]
+    [ValidateNotNullOrEmpty()]
+    [string[]] $Name = "all"
+  )
+
+  begin {
+    function Test-Administrator {
+      $currentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
+      $principal = New-Object Security.Principal.WindowsPrincipal($currentUser)
+      return $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    }
+
+    if (-not (Test-Administrator)) {
+      Write-Error -Message "This script must be executed as Administrator."
+      return
+    }
+
+    if (-not (Get-Module -Name "NetAdapter" -ErrorAction SilentlyContinue)) {
+      try {
+        Import-Module -Name "NetAdapter" -ErrorAction Stop
+      } catch {
+        Write-Error -Message "Unable to load the NetAdapter module."
+        return
+      }
+    }
+  }
+
+  process {
+    try {
+      if ($Name.ToLower() -eq "all") {
+        Disable-NetAdapterBinding -Name * -ComponentID ms_tcpip6 -PassThru -Confirm:$false -ErrorAction Stop
+      } else {
+        foreach ($adapter in $Name) {
+          Disable-NetAdapterBinding -Name $adapter -ComponentID "ms_tcpip6" -PassThru -Confirm:$false -ErrorAction Stop
+        }
+      }
+    } catch {
+      Write-Error -Message "An error occurred while enabling IPv6: $_"
+      return
+    }
+
+    try {
+      Get-NetAdapterBinding -Name * -ComponentID "ms_tcpip6"
+    } catch {
+      Write-Error -Message "An error occurred while retrieving NetAdapterBindings: $_"
+    }
+  }
 }
--- functions/Networking/Enable-IPv6.ps1
+++ functions/Networking/Enable-IPv6.ps1
@@ -1,54 +1,67 @@
 function Enable-IPv6 {
-	<#
-	.SYNOPSIS
-		Enable the IPv6 protocol
-	.DESCRIPTION
-		Enable the IPv6 protocol
-	.EXAMPLE
-		Enable-IPv6
-	.EXAMPLE
-		Enable-IPv6 -Name "Wi-Fi"
-	.EXAMPLE
-		Enable-IPv6 -AdapterName "Local Area Connection"
-	#>
-	[CmdletBinding()]
-	param (
-		[Alias("AdapterName")]
-		[string[]] $Name = "all"
-	)
+  <#
+  .SYNOPSIS
+  Enable the IPv6 protocol.
 
-	begin {
-		if (-not (Test-Administrator)) {
-			Write-Error "This script must be executed as Administrator."
-			Break
-		}
+  .DESCRIPTION
+  Enable the IPv6 protocol on specified network adapters or all adapters if no specific adapter is provided.
 
-		if (-not(Get-Module -Name "netadapter")) {
-			try {
-				Get-ModuleDependency -name "netadapter"
-			} catch {
-				Throw "Unable to load dependent module"
-				Break
-			}
-		}
-	}
+  .EXAMPLE
+  Enable-IPv6
 
-	process {
-		if ($Name.tolower() -eq "all") {
-			try {
-				Enable-NetAdapterBinding -Name * -ComponentID ms_tcpip6 -PassThru
-			} catch {
-				Throw "Unable to update all NetAdapterBindings"
-			}
-		} else {
-			$Name | ForEach-Object {
-				try {
-					Enable-NetAdapterBinding -Name $_ -ComponentID "ms_tcpip6" -PassThru
-				} catch {
-					Throw "Unable to update $_ NetAdapterBinding"
-				}
-			}
-		}
-		Get-NetAdapterBinding -Name * -ComponentID "ms_tcpip6"
-	}
+  .EXAMPLE
+  Enable-IPv6 -Name "Wi-Fi"
+
+  .EXAMPLE
+  Enable-IPv6 -Name "Local Area Connection"
+  #>
+  [CmdletBinding()]
+  param (
+    [Alias("AdapterName")]
+    [ValidateNotNullOrEmpty()]
+    [string[]] $Name = "all"
+  )
+
+  begin {
+    function Test-Administrator {
+      $currentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
+      $principal = New-Object Security.Principal.WindowsPrincipal($currentUser)
+      return $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    }
+
+    if (-not (Test-Administrator)) {
+      Write-Error -Message "This script must be executed as Administrator."
+      return
+    }
+
+    if (-not (Get-Module -Name "NetAdapter" -ErrorAction SilentlyContinue)) {
+      try {
+        Import-Module -Name "NetAdapter" -ErrorAction Stop
+      } catch {
+        Write-Error -Message "Unable to load the NetAdapter module."
+        return
+      }
+    }
+  }
+
+  process {
+    try {
+      if ($Name.ToLower() -eq "all") {
+        Enable-NetAdapterBinding -Name * -ComponentID ms_tcpip6 -PassThru -Confirm:$false -ErrorAction Stop
+      } else {
+        foreach ($adapter in $Name) {
+          Enable-NetAdapterBinding -Name $adapter -ComponentID ms_tcpip6 -PassThru -Confirm:$false -ErrorAction Stop
+        }
+      }
+    } catch {
+      Write-Error -Message "An error occurred while enabling IPv6: $_"
+      return
+    }
+
+    try {
+      Get-NetAdapterBinding -Name * -ComponentID ms_tcpip6
+    } catch {
+      Write-Error -Message "An error occurred while retrieving NetAdapterBindings: $_"
+    }
+  }
 }
