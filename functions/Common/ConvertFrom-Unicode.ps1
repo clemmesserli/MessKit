57a73ee583c5fdffd7fe6a328cad3db3c7d712ad
Clem Messerli <clemmesserli@gmail.com>
2024-06-22 14:51:36 -0500

    lint: convert tab to space indent and punctuation for better viewing consistency

--- functions/Common/ConvertFrom-Unicode.ps1
+++ functions/Common/ConvertFrom-Unicode.ps1
@@ -1,21 +1,21 @@
-Function ConvertFrom-Unicode {
+function ConvertFrom-Unicode {
     <#
-        .SYNOPSIS
-        Converts Unicode escape sequences in a string to their corresponding Unicode characters.
+    .SYNOPSIS
+    Converts Unicode escape sequences in a string to their corresponding Unicode characters.
 
-        .DESCRIPTION
-        This function takes a string as input and converts any Unicode escape sequences (e.g., \uXXXX) to their corresponding Unicode characters.
+    .DESCRIPTION
+    This function takes a string as input and converts any Unicode escape sequences (e.g., \uXXXX) to their corresponding Unicode characters.
 
-        .PARAMETER InputString
-        The string containing Unicode escape sequences to be converted.
+    .PARAMETER InputString
+    The string containing Unicode escape sequences to be converted.
 
-        .EXAMPLE
-        ConvertFrom-Unicode -InputString "Hello \u0048\u0065\u006C\u006C\u006F"
-        Output: "Hello Hello"
+    .EXAMPLE
+    ConvertFrom-Unicode -InputString "Hello \u0048\u0065\u006C\u006C\u006F"
+    Output: "Hello Hello"
 
-        .EXAMPLE
-        "Hello \u0048\u0065\u006C\u006C\u006F" | ConvertFrom-Unicode
-        Output: "Hello Hello"
+    .EXAMPLE
+    "Hello \u0048\u0065\u006C\u006C\u006F" | ConvertFrom-Unicode
+    Output: "Hello Hello"
     #>
     [CmdletBinding()]
     [OutputType([string])]
@@ -28,7 +28,7 @@ Function ConvertFrom-Unicode {
         $regex = [regex]'\\u(?<Value>[a-fA-F0-9]{4})'
     }
 
-    Process {
+    process {
         foreach ($string in $InputString) {
             $regex.Replace($string, {
                     param($match)
