2ae7865df6361e10fa339307be0dcaf3d5232bd8
Clem Messerli <clemmesserli@gmail.com>
2024-06-22 14:37:44 -0500

    lint: fix space and naming for better consistency

--- functions/Security/ConvertFrom-MorseCode.ps1
+++ functions/Security/ConvertFrom-MorseCode.ps1
@@ -1,62 +1,59 @@
-﻿Function ConvertFrom-MorseCode {
-	<#
-		.SYNOPSIS
-		Create plain text from morse code.
+﻿function ConvertFrom-MorseCode {
+  <#
+  .SYNOPSIS
+  Create plain text from morse code.
 
-		.DESCRIPTION
-		This script/function converts input strings to Morse Code.
-		It utilizes a dictionary mapping Morse Code symbols to characters for decoding.
+  .DESCRIPTION
+  This script/function converts input strings to Morse Code.
+  It utilizes a dictionary mapping Morse Code symbols to characters for decoding.
 
-		.EXAMPLE
-		ConvertFrom-MorseCode -String '-.-. .- .-.. .-.. / ----. .---- .---- -.-.--'
+  .EXAMPLE
+  ConvertFrom-MorseCode -String '-.-. .- .-.. .-.. / ----. .---- .---- -.-.--'
 
-		.EXAMPLE
-		ConvertFrom-MorseCode -String '.- -... -.-. .---- ..--- ...-- .--.-. .--. ----- .-- . .-. ... .... ...-- .-.. .-.. .-.-.- .-. ----- -.-. -.- ... -.-.--'
+  .EXAMPLE
+  ConvertFrom-MorseCode -String '.- -... -.-. .---- ..--- ...-- .--.-. .--. ----- .-- . .-. ... .... ...-- .-.. .-.. .-.-.- .-. ----- -.-. -.- ... -.-.--'
 
-		.EXAMPLE
-		ConvertFrom-MorseCode -String '.--. ----- .-- . .-. ... .... ...-- .-.. .-.. / .-. ----- -.-. -.- ... -.-.--' | Set-Clipboard
+  .EXAMPLE
+  ConvertFrom-MorseCode -String '.--. ----- .-- . .-. ... .... ...-- .-.. .-.. / .-. ----- -.-. -.- ... -.-.--' | Set-Clipboard
 
-		.EXAMPLE
-		ConvertFrom-MorseCode -String (Get-Content ./private/MorseCode.txt) | Out-File ./private/MorsePlain.txt
+  .EXAMPLE
+  ConvertFrom-MorseCode -String (Get-Content ./private/MorseCode.txt) | Out-File ./private/MorsePlain.txt
+  #>
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory, ValueFromPipeline)]
+    [AllowEmptyString()]
+    [string[]]$String
+  )
 
-		.NOTES
-		Enhanced by Codiumate
-    #>
-	[CmdletBinding()]
-	param(
-		[Parameter(Mandatory, ValueFromPipeline)]
-		[AllowEmptyString()]
-		[string[]]$String
-	)
+  process {
+    $morseToLetter = @{
+      # Upper-Case Letters
+      ".-" = "A"; "-..." = "B"; "-.-." = "C"; "-.." = "D"; "." = "E"
+      "..-." = "F"; "--." = "G"; "...." = "H"; ".." = "I"; ".---" = "J"
+      "-.-" = "K"; ".-.." = "L"; "--" = "M"; "-." = "N"; "---" = "O"
+      ".--." = "P"; "--.-" = "Q"; ".-." = "R"; "..." = "S"; "-" = "T"
+      "..-" = "U"; "...-" = "V"; ".--" = "W"; "-..-" = "X"; "-.--" = "Y"
+      "--.." = "Z"
 
-	process {
-		$morseToLetter = @{
-			# Upper-Case Letters
-			".-" = "A"; "-..." = "B"; "-.-." = "C"; "-.." = "D"; "." = "E"
-			"..-." = "F"; "--." = "G"; "...." = "H"; ".." = "I"; ".---" = "J"
-			"-.-" = "K"; ".-.." = "L"; "--" = "M"; "-." = "N"; "---" = "O"
-			".--." = "P"; "--.-" = "Q"; ".-." = "R"; "..." = "S"; "-" = "T"
-			"..-" = "U"; "...-" = "V"; ".--" = "W"; "-..-" = "X"; "-.--" = "Y"
-			"--.." = "Z"
+      # Numbers
+      "-----" = "0"; ".----" = "1"; "..---" = "2"; "...--" = "3"; "....-" = "4"
+      "....." = "5"; "-...." = "6"; "--..." = "7"; "---.." = "8"; "----." = "9"
 
-			# Numbers
-			"-----" = "0"; ".----" = "1"; "..---" = "2"; "...--" = "3"; "....-" = "4"
-			"....." = "5"; "-...." = "6"; "--..." = "7"; "---.." = "8"; "----." = "9"
+      # Symbols
+      "-.-.--" = "!"; ".--.-." = "@"; ".-..." = "&"; "-...-" = "="
+      ".-.-." = "+"; "-....-" = "-"; "-..-." = "/"; ".-.-.-" = "."
+      "--..--" = ","; "---..." = ":"; "-.--." = "("; "-.--.-" = ")"
+      ".----." = "`'"; ".-..-." = "`""; "..--.." = "?"; "/" = " "
+    }
 
-			# Symbols
-			"-.-.--" = "!"; ".--.-." = "@"; ".-..." = "&"; "-...-" = "="
-			".-.-." = "+"; "-....-" = "-"; "-..-." = "/"; ".-.-.-" = "."
-			"--..--" = ","; "---..." = ":"; "-.--." = "("; "-.--.-" = ")"
-			".----." = "`'"; ".-..-." = "`""; "..--.." = "?"; "/" = " "
-		}
-
-		$stringBuilder = [System.Text.StringBuilder]::new()
-		foreach ($s in $String) {
-			foreach ($char in $s.Split(' ')) {
-				$letter = $morseToLetter[$char]
-				[void]$stringBuilder.Append($letter)
-			}
-			$stringBuilder.ToString()
-		}
-	}
+    $stringBuilder = [System.Text.StringBuilder]::new()
+    foreach ($s in $String) {
+      foreach ($char in $s.Split(' ')) {
+        $letter = $morseToLetter[$char]
+        [void]$stringBuilder.Append($letter)
+      }
+      $stringBuilder.ToString()
+    }
+  }
 }
--- functions/Security/ConvertTo-MorseCode.ps1
+++ functions/Security/ConvertTo-MorseCode.ps1
@@ -1,89 +1,86 @@
-﻿Function ConvertTo-MorseCode {
-	<#
-		.SYNOPSIS
-		Converts input string to Morse Code and optionally plays it as audio.
+﻿function ConvertTo-MorseCode {
+  <#
+  .SYNOPSIS
+  Converts input string to Morse Code and optionally plays it as audio.
 
-		.DESCRIPTION
-		This script/function converts input strings to Morse Code and optionally plays it as audio.
-		It utilizes a dictionary mapping characters to Morse Code symbols for encoding.
+  .DESCRIPTION
+  This script/function converts input strings to Morse Code and optionally plays it as audio.
+  It utilizes a dictionary mapping characters to Morse Code symbols for encoding.
 
-		.EXAMPLE
-		ConvertTo-MorseCode -String 'SOS'
+  .EXAMPLE
+  ConvertTo-MorseCode -String 'SOS'
 
-		.EXAMPLE
-		ConvertTo-MorseCode -String 'Call (123) 456-7689 - I Have Fallen and Cannot Get Up.' -audio
+  .EXAMPLE
+  ConvertTo-MorseCode -String 'Call (123) 456-7689 - I Have Fallen and Cannot Get Up.' -audio
 
-		.EXAMPLE
-		ConvertTo-MorseCode -String 'abc123@p0wersh3ll.r0cks!' | Set-Clipboard
+  .EXAMPLE
+  ConvertTo-MorseCode -String 'abc123@p0wersh3ll.r0cks!' | Set-Clipboard
 
-		.EXAMPLE
-		ConvertTo-MorseCode -String (Get-Content ./private/MorsePlain.txt) | Out-File ./private/MorseCode.txt
+  .EXAMPLE
+  ConvertTo-MorseCode -String (Get-Content ./private/MorsePlain.txt) | Out-File ./private/MorseCode.txt
 
-		.EXAMPLE
-		'abc123@p0wersh3ll.r0cks!' | ConvertTo-MorseCode -audio
+  .EXAMPLE
+  'abc123@p0wersh3ll.r0cks!' | ConvertTo-MorseCode -audio
+  #>
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory, ValueFromPipeline)]
+    [AllowEmptyString()]
+    [string[]]$String,
 
-		.NOTES
-		Enhanced by Codiumate
-    #>
-	[CmdletBinding()]
-	param(
-		[Parameter(Mandatory, ValueFromPipeline)]
-		[AllowEmptyString()]
-		[string[]]$String,
+    [Parameter()]
+    [switch]$audio
+  )
 
-		[Parameter()]
-		[switch]$audio
-	)
+  process {
+    $letterToMorse = @{
+      # Upper-Case Letters
+      "A" = ".-"; "B" = "-..."; "C" = "-.-."; "D" = "-.."; "E" = "."
+      "F" = "..-."; "G" = "--."; "H" = "...."; "I" = ".."; "J" = ".---"
+      "K" = "-.-"; "L" = ".-.."; "M" = "--"; "N" = "-."; "O" = "---"
+      "P" = ".--."; "Q" = "--.-"; "R" = ".-."; "S" = "..."; "T" = "-"
+      "U" = "..-"; "V" = "...-"; "W" = ".--"; "X" = "-..-"; "Y" = "-.--"
+      "Z" = "--.."
 
-	process {
-		$letterToMorse = @{
-			# Upper-Case Letters
-			"A" = ".-"; "B" = "-..."; "C" = "-.-."; "D" = "-.."; "E" = "."
-			"F" = "..-."; "G" = "--."; "H" = "...."; "I" = ".."; "J" = ".---"
-			"K" = "-.-"; "L" = ".-.."; "M" = "--"; "N" = "-."; "O" = "---"
-			"P" = ".--."; "Q" = "--.-"; "R" = ".-."; "S" = "..."; "T" = "-"
-			"U" = "..-"; "V" = "...-"; "W" = ".--"; "X" = "-..-"; "Y" = "-.--"
-			"Z" = "--.."
+      # Numbers
+      "0" = "-----"; "1" = ".----"; "2" = "..---"; "3" = "...--"; "4" = "....-"
+      "5" = "....."; "6" = "-...."; "7" = "--..."; "8" = "---.."; "9" = "----."
 
-			# Numbers
-			"0" = "-----"; "1" = ".----"; "2" = "..---"; "3" = "...--"; "4" = "....-"
-			"5" = "....."; "6" = "-...."; "7" = "--..."; "8" = "---.."; "9" = "----."
+      # Symbols
+      "!" = "-.-.--"; "@" = ".--.-."; "&" = ".-..."; "=" = "-...-"
+      "+" = ".-.-."; "-" = "-....-"; "/" = "-..-."; "." = ".-.-.-"
+      "," = "--..--"; ":" = "---..."; "(" = "-.--."; ")" = "-.--.-"
+      "`'" = ".----."; "`"" = ".-..-."; "?" = "..--.."; " " = "/"
+    }
 
-			# Symbols
-			"!" = "-.-.--"; "@" = ".--.-."; "&" = ".-..."; "=" = "-...-"
-			"+" = ".-.-."; "-" = "-....-"; "/" = "-..-."; "." = ".-.-.-"
-			"," = "--..--"; ":" = "---..."; "(" = "-.--."; ")" = "-.--.-"
-			"`'" = ".----."; "`"" = ".-..-."; "?" = "..--.."; " " = "/"
-		}
+    $data = @()
+    $line = @()
+    $String = $String.ToUpper()
+    foreach ($char in $String.ToCharArray()) {
+      $morseCode = $letterToMorse[[string]$char]
+      $line += $morseCode
+    }
+    $data += $line -join (' ')
 
-		$data = @()
-		$line = @()
-		$String = $String.ToUpper()
-		foreach ($char in $String.ToCharArray()) {
-			$morseCode = $letterToMorse[[string]$char]
-			$line += $morseCode
-		}
-		$data += $line -join (' ')
+    if ($audio.IsPresent) {
+      Write-Output ($data -join (' / '))
+      # Initial beep
+      [Console]::Beep(37, 1000)
 
-		if ($audio.IsPresent) {
-			Write-Output ($data -join (' / '))
-			# Initial beep
-			[Console]::Beep(37, 1000)
-
-			foreach ($d in $data.ToCharArray()) {
-				switch ($d) {
-					'.' { [Console]::Beep(800, 100) }
-					'-' { [Console]::Beep(800, 300) }
-					'/' { [Console]::Beep(37, 600) }
-					' ' { Start-Sleep -Milliseconds 600 }
-					default {
-						Write-Warning "Unknown char: $_"
-						[Console]::Beep(2000, 1000)
-					}
-				}
-			}
-		} else {
-			return $data -join (' / ')
-		}
-	}
+      foreach ($d in $data.ToCharArray()) {
+        switch ($d) {
+          '.' { [Console]::Beep(800, 100) }
+          '-' { [Console]::Beep(800, 300) }
+          '/' { [Console]::Beep(37, 600) }
+          ' ' { Start-Sleep -Milliseconds 600 }
+          default {
+            Write-Warning "Unknown char: $_"
+            [Console]::Beep(2000, 1000)
+          }
+        }
+      }
+    } else {
+      return $data -join (' / ')
+    }
+  }
 }
