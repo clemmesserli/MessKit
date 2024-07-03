804a37ffe8b2eb034079a7e65250b4bb6f5f90df
Clem Messerli <clemmesserli@gmail.com>
2024-07-02 19:51:37 -0500

    edit: param name consistency and option to display or save to file

--- functions/Security/Protect-MySecret.ps1
+++ functions/Security/Protect-MySecret.ps1
@@ -1,31 +1,33 @@
 function Protect-MySecret {
   <#
   .SYNOPSIS
-  Encrypts a secret using a passphrase and saves it to a file.
+  Encrypts a secret using a key input in the form of a SecureString and saves it to a file.
 
   .DESCRIPTION
-  This function takes a secret string and a secure passphrase, encrypts the secret,
-  and saves the encrypted content to a file.
+  This function takes plain text content and a Key in the form of a SecureString which is then used to encrypt the content.
+  The output can then be saved to a file or displayed on screen.
 
   .PARAMETER Secret
   The secret text to encrypt.
 
-  .PARAMETER Passphrase
-  The passphrase used for encryption, as a SecureString.
+  .PARAMETER Key
+  The key value used for encryption, as a SecureString.
 
   .PARAMETER OutputPath
   The path where the encrypted secret will be saved. Defaults to a file named 'secret.txt' in the temp directory.
 
   .EXAMPLE
-  $securePass = ConvertTo-SecureString "My secure passphrase" -AsPlainText -Force
-  Protect-MySecret -Secret "My super secret" -Passphrase $securePass
+  $PassPhrase = Read-Host "Enter passphrase" -AsSecureString
+  "Another secret" | Protect-MySecret -Passphrase $PassPhrase -FilePath "$env:tmp\encrypted.txt"
+  Get-Content "$env:tmp\encrypted.txt"
 
   .EXAMPLE
-  $securePass = Read-Host "Enter passphrase" -AsSecureString
-  "Another secret" | Protect-MySecret -Passphrase $securePass -OutputPath "C:\Secrets\encrypted.txt"
+  $PassPhrase = ConvertTo-SecureString "Good Boy, Duke!" -AsPlainText -Force
+  Protect-MySecret -Secret "My super secret" -Passphrase $PassPhrase -DisplaySecret
+
 
   .EXAMPLE
-  $securePass = ConvertTo-SecureString "Good Boy, Duke!" -AsPlainText -Force
+  $PassPhrase = ConvertTo-SecureString "Good Boy, Duke!" -AsPlainText -Force
   $content = @"
   Bush Baked Beans Secret Family Recipe
   Ingredients:
@@ -37,21 +39,24 @@ function Protect-MySecret {
   1. Preheat oven to 350 degrees
   2. Pray as you are still missing ingredients :)
 "@
-  $content | Protect-MySecret -Passphrase $securePass -OutputPath "$env:TEMP\secret.txt"
+  $content | Protect-MySecret -Passphrase $PassPhrase -FilePath "$env:TEMP\secret.txt"
 
   .NOTES
   The secret is encrypted using AES encryption with a 256-bit key derived from the passphrase.
   #>
   [CmdletBinding()]
   param (
-    [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+    [Parameter(Mandatory, ValueFromPipeline)]
     [string]$Secret,
 
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory)]
     [SecureString]$Passphrase,
 
-    [Parameter(Mandatory = $false)]
-    [string]$OutputPath = "$env:TEMP\secret.txt"
+    [Parameter()]
+    [string]$FilePath,
+
+    [Parameter()]
+    [switch]$DisplaySecret
   )
 
   $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Passphrase)
@@ -63,6 +68,12 @@ function Protect-MySecret {
   $encryptedSecret = $Secret | ConvertTo-SecureString -AsPlainText -Force |
     ConvertFrom-SecureString -Key $key
 
-  Set-Content -Path $OutputPath -Value $encryptedSecret -NoNewline
-  Write-Host "Secret encrypted and saved to: $OutputPath"
+  if ($DisplaySecret) {
+    return $encryptedSecret
+  } elseif ($FilePath) {
+    Set-Content -Path $FilePath -Value $encryptedSecret -NoNewline
+    Write-Host "Secret encrypted and saved to: $FilePath"
+  } else {
+    Write-Host "Use -DisplaySecret to display on screen or -FilePath to save to a file."
+  }
 }
