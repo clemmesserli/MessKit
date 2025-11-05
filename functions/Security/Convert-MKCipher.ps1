b5564955bae0a0aee706b1cb5031b39475c99a5a
Clem Messerli <clem.messerli@transamerica.com>
2025-11-05 16:09:18 -0600

    edit: update default path to user directory

--- functions/Security/Convert-MKCipher.ps1
+++ functions/Security/Convert-MKCipher.ps1
@@ -1,5 +1,5 @@
 function Convert-MKCipher {
-  <#
+	<#
   .SYNOPSIS
   Returns SSL Cipher name equivalent between IANA and OpenSSL
 
@@ -27,76 +27,76 @@ function Convert-MKCipher {
   Requires OpenSSL version 1.1.1 or higher.
   If you have the latest version of Git, it should include a recent version of openssl.exe.
   #>
-  [CmdletBinding()]
-  [OutputType([string])]
-  param (
-    [Parameter(Mandatory, ValueFromPipeline)]
-    [ValidateNotNullOrEmpty()]
-    [string]$Cipher,
+	[CmdletBinding()]
+	[OutputType([string])]
+	param (
+		[Parameter(Mandatory, ValueFromPipeline)]
+		[ValidateNotNullOrEmpty()]
+		[string]$Cipher,
 
-    [Parameter()]
-    [ValidateScript({
-        if (Test-Path $_ -PathType Leaf) {
-          $opensslVersion = & $_ version
-          if ($opensslVersion -notlike 'OpenSSL 1.1.1*') {
-            throw 'Invalid OpenSSL version. Please provide a path to OpenSSL version 1.1.1 or higher.'
-          }
-          return $true
-        }
-        throw 'The specified path does not exist or is not a file.'
-      })]
-    #[string]$ExePath = (Get-MyParam).'Convert-MKCipher'.exePath
-    [string]$ExePath = 'C:\Program Files\Git\mingw64\bin\openssl.exe'  # Default path if not provided
-  )
+		[Parameter()]
+		[ValidateScript({
+				if (Test-Path $_ -PathType Leaf) {
+					$opensslVersion = & $_ version
+					if ($opensslVersion -notlike 'OpenSSL 1.1.1*') {
+						throw 'Invalid OpenSSL version. Please provide a path to OpenSSL version 1.1.1 or higher.'
+					}
+					return $true
+				}
+				throw 'The specified path does not exist or is not a file.'
+			})]
+		#[string]$ExePath = (Get-MyParam).'Convert-MKCipher'.exePath
+		[string]$ExePath = '$env:LOCALAPPDATA\Programs\Git\mingw64\bin\openssl.exe'  # Default path if not provided
+	)
 
-  begin {
-    # Initialize cache
-    if (-not $script:CipherCache) {
-      $script:CipherCache = @{}
-    }
-  }
+	begin {
+		# Initialize cache
+		if (-not $script:CipherCache) {
+			$script:CipherCache = @{}
+		}
+	}
 
-  process {
-    # Check cache first
-    if ($script:CipherCache.ContainsKey($Cipher)) {
-      Write-Verbose "Returning cached result for $Cipher"
-      return $script:CipherCache[$Cipher]
-    }
+	process {
+		# Check cache first
+		if ($script:CipherCache.ContainsKey($Cipher)) {
+			Write-Verbose "Returning cached result for $Cipher"
+			return $script:CipherCache[$Cipher]
+		}
 
-    try {
-      $result = $null
-      Write-Verbose "Executing OpenSSL command: $ExePath ciphers -stdname"
-      $opensslOutput = & "$ExePath" ciphers -stdname 2>&1
+		try {
+			$result = $null
+			Write-Verbose "Executing OpenSSL command: $ExePath ciphers -stdname"
+			$opensslOutput = & "$ExePath" ciphers -stdname 2>&1
 
-      if ($LASTEXITCODE -ne 0) {
-        throw "OpenSSL command failed with exit code $LASTEXITCODE"
-        return
-      }
+			if ($LASTEXITCODE -ne 0) {
+				throw "OpenSSL command failed with exit code $LASTEXITCODE"
+				return
+			}
 
-      #OpenSSL to IANA
-      $ianaMatch = $opensslOutput | Select-String "\s$Cipher\s" | Select-Object -First 1
-      if ($ianaMatch) {
-        $result = $ianaMatch.Line.split(' ')[0]
-        Write-Verbose "Found IANA match: $result"
-      }
+			#OpenSSL to IANA
+			$ianaMatch = $opensslOutput | Select-String "\s$Cipher\s" | Select-Object -First 1
+			if ($ianaMatch) {
+				$result = $ianaMatch.Line.split(' ')[0]
+				Write-Verbose "Found IANA match: $result"
+			}
 
-      if (-not $result) {
-        #IANA to OpenSSL
-        $opensslMatch = $opensslOutput | Select-String "^$Cipher\s" | Select-Object -First 1
-        if ($opensslMatch) {
-          $result = $opensslMatch.Line.Split(' ')[2]
-          Write-Verbose "Found OpenSSL match: $result"
-        }
-      }
+			if (-not $result) {
+				#IANA to OpenSSL
+				$opensslMatch = $opensslOutput | Select-String "^$Cipher\s" | Select-Object -First 1
+				if ($opensslMatch) {
+					$result = $opensslMatch.Line.Split(' ')[2]
+					Write-Verbose "Found OpenSSL match: $result"
+				}
+			}
 
-      if ($result) {
-        $script:CipherCache[$Cipher] = $result
-        return $result
-      } else {
-        Write-Warning "No matching cipher found for [$Cipher]"
-      }
-    } catch {
-      Write-Error "An error occurred: $_"
-    }
-  }
+			if ($result) {
+				$script:CipherCache[$Cipher] = $result
+				return $result
+			} else {
+				Write-Warning "No matching cipher found for [$Cipher]"
+			}
+		} catch {
+			Write-Error "An error occurred: $_"
+		}
+	}
 }
